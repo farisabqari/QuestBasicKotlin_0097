e6e8625242d1d8860b4273022930207adc48f1b7
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-10-13 03:48:07 +0800

    "Menambahkan fungsi tanpa parameter dan dengan parameter untuk menyapa pengguna"

---
+++ app/src/main/java/com/example/pam_097/Functions.kt
@@ -0,0 +1,13 @@
+package com.example.pam_097
+
+
+fun withoutParameter() {
+    println("= without Parameter ==")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
