c65351116c682560f459667594b123961ae2ffe9
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-10-13 03:59:22 +0800

    Menambahkan contoh pernyataan when untuk nama hari dan try-catch untuk konversi string ke integer

--- app/src/main/java/com/example/pam_097/ConditionalExpression.kt
+++ app/src/main/java/com/example/pam_097/ConditionalExpression.kt
@@ -12,4 +12,23 @@ fun ConditionalStatement() {
 
     }
 
+    val day = 1
+    when (day){
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Invalid Fay")
+    }
+    val value ="10"
+    try {
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException){
+        println("Invalid Number")
+    }
+
 }
