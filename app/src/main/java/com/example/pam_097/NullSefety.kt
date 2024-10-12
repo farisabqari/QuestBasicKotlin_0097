0cbf5170302402252d574e0a5c2259240bacfa19
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-10-13 03:56:35 +0800

    Menambah contoh penggunaan null safety dan panjang string di Kotlin

---
+++ app/src/main/java/com/example/pam_097/NullSefety.kt
@@ -0,0 +1,26 @@
+package com.example.pam_097
+
+fun nullSafety(){
+    var neverNull: String = "This can't be null"
+
+    var nullable: String?  = "you can keep a null here"
+    nullable = null
+
+    if (neverNull == null) {
+        println("inferredNonNull is null")
+
+    }else{
+        println("inferredNonNull is not null")
+
+        println(neverNull.length)
+        println(nullable?.length)
+    }
+    fun strLength(notNull: String): Int{
+        return notNull.length
+    }
+    println(strLength(neverNull))
+}
+
+fun main (){
+    nullSafety()
+}
