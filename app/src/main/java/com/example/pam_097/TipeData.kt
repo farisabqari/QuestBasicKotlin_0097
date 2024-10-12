8684b76d8e901817117f7a2909e8ddf351637f70
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-10-13 03:19:50 +0800

    Menambahkan kode untuk contoh penggunaan tipe data byte, short, dan int

--- app/src/main/java/com/example/pam_097/TipeData.kt
+++ app/src/main/java/com/example/pam_097/TipeData.kt
@@ -1,2 +1,13 @@
 package com.example.pam_097
 
+fun main() {
+    //byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    val shortValue: Short = 20
+    println("Short: $shortValue")
+
+    val intValue: Int = 30
+    println("Int: $intValue")
+}
