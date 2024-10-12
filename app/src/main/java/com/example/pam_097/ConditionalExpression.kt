82e23caf7264374c30388f7fdd6cdf8fe0918e3b
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-10-13 03:58:25 +0800

    Menambah contoh pernyataan kondisi untuk menentukan angka positif atau negatif

---
+++ app/src/main/java/com/example/pam_097/ConditionalExpression.kt
@@ -0,0 +1,15 @@
+package com.example.pam_097
+
+fun ConditionalStatement() {
+    println("=== Conditional Expression ===")
+
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+
+    } else {
+        println("Negative Number")
+
+    }
+
+}
